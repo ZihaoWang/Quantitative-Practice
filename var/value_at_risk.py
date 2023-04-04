9a33e77e738337c69ffaa0f51091e86cc81d28dd
evensong <evensong@MacBook-Pro-4.local>
2023-04-04 23:20:13 +0200

    finish VaR

--- var/value_at_risk.py
+++ var/value_at_risk.py
@@ -29,13 +29,13 @@ class VaR_Backtest(object):
         print(f"mu = {self.mu}, 1 day sigma = {self.sigma}, 10 days sigma = {self.sigma_10day}, 1 year sigma = {self.sigma_year}")
 
     def comp_breach(self):
-        var_10days = self.__comp_var_10days()
-        forward_return_10days = self.__comp_forward_return_10days()
+        self.__comp_var_10days()
+        self.__comp_forward_return_10days()
         
-        assert len(var_10days) == 10 + len(forward_return_10days)
-        var_10days = var_10days[:-10]
+        assert len(self.var_10days) == 10 + len(self.forward_return_10days)
+        self.var_10days = self.var_10days[:-10]
 
-        all_breaches = forward_return_10days < var_10days
+        all_breaches = self.forward_return_10days < self.var_10days
         n_breach = np.sum(all_breaches)
         percent_breach = n_breach / all_breaches.shape[0]
 
@@ -48,23 +48,31 @@ class VaR_Backtest(object):
         return n_breach, percent_breach, n_consecutive_breach, percent_consecutive_breach
 
     def plot_backtest(self):
+        valid_date = self.date[n_backtest : n_backtest + len(self.forward_return_10days)]
+        n_tick_date = 8
+        tick_inverval = len(valid_date) // n_tick_date
+        tick_dates = [valid_date[i * tick_inverval] for i in range(n_tick_date)]
+
         figure = plt.figure(num = 1, figsize = (10, 6))
         ax = figure.add_subplot(111)
-        ax.hist(self.log_return, bins = 100)
-        ax.set_title("Histogram of daily log returns")
+        ax.plot(valid_date, self.var_10days, label = "10 day VaR")
+        ax.plot(valid_date, self.forward_return_10days, label = "10 day realized")
+        ax.set_title("VaR backtesting")
+        ax.xaxis.set_ticks(np.arange(0, len(valid_date), tick_inverval))
+        ax.legend()
         plt.show()
 
     def __comp_var_10days(self, percentile = 0.99):
         factor = -scipy.stats.norm.ppf(percentile) # inverse Gaussian CDF
 
-        var_10days = []
+        self.var_10days = []
         for i_begin in range(1, len(self.log_return)):
             i_end = i_begin + self.n_backtest
             if i_end >= len(self.log_return):
                 break
 
             var = np.std(self.log_return[i_begin : i_end], ddof = 1) * np.sqrt(10) * factor
-            var_10days.append(var)
+            self.var_10days.append(var)
             '''
             if i_begin == 1203:
                 print(len(self.date), self.date[i_begin : i_end])
@@ -75,20 +83,16 @@ class VaR_Backtest(object):
                 print(var)
                 exit()
             '''
-        var_10days = np.array(var_10days)
-
-        return var_10days
+        self.var_10days = np.array(self.var_10days)
             
     def __comp_forward_return_10days(self):
-        forward_return_10days = []
+        self.forward_return_10days = []
         for i_begin in range(1 + self.n_backtest, len(self.log_return)):
             i_end = i_begin + 10
             if i_end >= len(self.log_return):
                 break
 
-            forward_return_10days.append(np.log(self.sp500[i_end] / self.sp500[i_begin]))
-
-        return forward_return_10days
+            self.forward_return_10days.append(np.log(self.sp500[i_end] / self.sp500[i_begin]))
 
     def read_data(self):
         with open('./data/sp500.csv', "r") as f_src:
@@ -98,12 +102,6 @@ class VaR_Backtest(object):
 
                 l = l.strip().split(",")
                 self.date.append(l[0])
-        figure = plt.figure(num = 1, figsize = (10, 6))
-        ax = figure.add_subplot(111)
-        ax.hist(self.log_return, bins = 100)
-        ax.set_title("Histogram of daily log returns")
-        plt.show()
-
                 self.sp500.append(float(l[1]))
         print(f"finish reading {len(self.sp500)} data")
 
@@ -120,5 +118,6 @@ if __name__ == "__main__":
     model = VaR_Backtest(n_backtest)
     #model.plot_histogram()
     n_breach, percent_breach, n_consecutive_breach, percent_consecutive_breach = model.comp_breach()
-    print(n_breach, percent_breach, n_consecutive_breach, percent_consecutive_breach)
+    #print(n_breach, percent_breach, n_consecutive_breach, percent_consecutive_breach)
+    model.plot_backtest()
 
