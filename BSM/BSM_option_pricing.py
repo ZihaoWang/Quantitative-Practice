f664543e896aa8482b0bc0a3c34e1b6cfdcefb82
evensong <evensong@MacBook-Pro-172.local>
2023-04-26 00:52:57 +0200

    closed form and MC stock prices

--- BSM/BSM_option_pricing.py
+++ BSM/BSM_option_pricing.py
@@ -77,6 +77,51 @@ class BSM_EurOption(BSM_BaseModel):
 
         return self.greeks
 
+class BSM_AsiaOption(BSM_BaseModel):
+    def __init__(self, pr_spot, pr_strike, interest, day_expire, vol):
+        super(BSM_AsiaOption, self).__init__(pr_spot, pr_strike, interest, day_expire, vol)
+        a = self.vol * np.sqrt(self.day_expire)
+        self.d1 = (np.log(self.pr_spot / self.pr_strike) + (self.interest + np.power(self.vol, 2) / 2) * self.day_expire) / a
+        self.d2 = self.d1 - a
+        self.decay_exp = np.exp(-self.interest * self.day_expire)
+
+    def comp_option_prices(self):
+        if np.abs(self.vol) < 1e-6 or np.abs(self.day_expire) < 1e-6:
+            self.option_prices["pr_call"] = np.maximum(0.0, self.pr_spot - self.pr_strike)
+            self.option_prices["pr_put"] = np.maximum(0.0, self.pr_strike - self.pr_spot)
+        else:
+            self.option_prices["pr_call"] = self.pr_spot * Normal.cdf(self.d1) - self.pr_strike * self.decay_exp * Normal.cdf(self.d2)
+            self.option_prices["pr_put"] = self.pr_strike * self.decay_exp * Normal.cdf(-self.d2) - self.pr_spot * Normal.cdf(-self.d1)
+
+        return self.option_prices
+
+    def comp_greeks(self):
+        if np.abs(self.vol) < 1e-6 or np.abs(self.day_expire) < 1e-6:
+            self.greeks["delta_call"] = 1.0 if self.pr_spot > self.pr_strike else 0.0
+            self.greeks["delta_put"] = -1.0 if self.pr_spot < self.pr_strike else 0.0
+        else:
+            self.greeks["delta_call"] = Normal.cdf(self.d1)
+            self.greeks["delta_put"] = -Normal.cdf(-self.d1)
+
+        tmp1 = -self.pr_spot * Normal.pdf(self.d1) * self.vol / (2 * np.sqrt(self.day_expire))
+        tmp2 = self.interest * self.pr_strike * self.decay_exp
+        self.greeks["theta_call"] = (tmp1 - tmp2 * Normal.cdf(self.d2)) / 365
+        self.greeks["theta_put"] = (tmp1 + tmp2 * Normal.cdf(-self.d2)) / 365
+
+        tmp3 = self.pr_strike * self.day_expire * self.decay_exp / 100
+        self.greeks["rho_call"] = tmp3 * Normal.cdf(self.d2)
+        self.greeks["rho_put"] = -tmp3 * Normal.cdf(-self.d2)
+
+        if np.abs(self.vol) < 1e-6 or np.abs(self.day_expire) < 1e-6:
+            self.greeks["vega"] = 0.0
+        else:
+            self.greeks["vega"] = self.pr_spot * Normal.pdf(self.d1) * np.sqrt(self.day_expire) / 100
+
+        self.greeks["gamma"] = Normal.pdf(self.d1) / (self.pr_spot * self.vol * np.sqrt(self.day_expire))
+
+        return self.greeks
+
+
 if __name__ == "__main__":
     pr_spot = 100
     pr_strike = 100
